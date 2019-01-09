7d19c8ba72236249fedb01b67e0f70508a3887a6
kevin.chen <don-pan.cwf@163.com>
2019-01-09 22:14:57 +0800

    安装bodyparser 且连接本地数据库

---
+++ config/key.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI:'mongodb://localhost:27017/local',
+    // mongoURI:"mongodb://test:bai123456@ds063150.mlab.com:63150/api",
+    secretName:"secret"
+}
--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const exphbs  = require('express-handlebars');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const db = require('./config/key').mongoURI;
 
 const app = express();
 const port = 8000;
+
+mongoose.connect(db,{ useNewUrlParser: true }).then( () => console.log('DB Successful！')).catch((err) => console.log(err));
 
 //中间件
 app.engine('handlebars', exphbs({
@@ -10,11 +16,16 @@ app.engine('handlebars', exphbs({
 }));
 app.set('view engine', 'handlebars');
 app.set('views', __dirname + '/views');
+app.use(bodyParser.urlencoded({extended:false}))  
+app.use(bodyParser.json());
 
 
 //router
 app.get('/',(req,res) => {
-    res.render('index')
+    const title = '这里可以传参';
+    res.render('index',{
+        title:title
+    })
 });
 app.get('/about',(req,res) => {
     res.render('about')
